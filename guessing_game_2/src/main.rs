24b3fdfcf9202210a0e98fd649db0aa20c95e2b1
LancemDev <munyaolance1@gmail.com>
2023-12-19 15:15:53 +0300

    The game is now more realistic and loops until you get an answer right

--- guessing_game_2/src/main.rs
+++ guessing_game_2/src/main.rs
@@ -1,24 +1,40 @@
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
-
     // Creates a mutable variable guess and makes it a new String instance
     let mut guess = String::new();
     // let x = 1;
     // let y = 2;
-    println!("--Guessing game--");
-    println!("Enter a random number");
+        println!("--Guessing game--");
+    loop{
+        guess.clear();
 
-    //Reads input and stores it in the mutable var guess
-    io::stdin().read_line(&mut guess)
-    .expect("Failed to do the thing");
+        println!("Enter a random number");
 
-    // I believe this a functions to the random crate for the version I'm using
-    let secret = rand::thread_rng()
-    // For a different random crate version there is a possibility that the syntax will change in the arguments
-    .gen_range(1, 101);
+        //Reads input and stores it in the mutable var guess
+        io::stdin().read_line(&mut guess)
+        .expect("Failed to do the thing");
 
-    println!("The secret number is: {}", secret);
-    // println!("x = {}, y = {}", y, x); // Notice sequence of variables
+        let guess: u32 = guess.trim().parse()
+        .expect("Please type in a number");
+
+        // I believe this a functions to the random crate for the version I'm using
+        let secret = rand::thread_rng()
+        // For a different rand crate version there is a possibility that the syntax will change in the arguments. For this I'm using "0.8.12"
+        .gen_range(1, 101);
+
+        println!("The secret number is: {}", secret);
+        // println!("x = {}, y = {}", y, x); // Notice sequence of variables
+
+        match guess.cmp(&secret){
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You guessed it right man");
+                break;
+            }
+        }
+    }
 }
